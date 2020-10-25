dc1f5e525309aabf5b20f98e3692e5a7610152c9
Reshmi-Anwar <ranwar10@stuy.edu>
2020-10-25 12:10:44 -0400

    finished largest method

--- ArrayOps.java
+++ ArrayOps.java
@@ -6,4 +6,14 @@ public class ArrayOps{
     }
     return sum;
   }
+
+  public static int largest(int[] arr){
+    int max = arr[0];
+    for(int i = 0; i < arr.length; i++){
+      if(arr[i] > max){
+        max = arr[i];
+      }
+    }
+    return max;
+  }
 }
